55b47fc49dc4919ab9443074824402fddb039088
Eric Bouchut <ebouchut@gmail.com>
2024-06-15 19:22:50 +0200

    🔧 Nvim: Add keymaps for nvim-tree file explorer
    h Close the folder/file

--- tag-nvim/config/nvim/lua/ebouchut/plugins/nvim-tree.lua
+++ tag-nvim/config/nvim/lua/ebouchut/plugins/nvim-tree.lua
@@ -8,7 +8,22 @@ return {
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
 
+    local function my_on_attach(bufnr)
+      local api = require("nvim-tree.api")
+
+      local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+      end
+
+      api.config.mappings.default_on_attach(bufnr)
+
+      vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
+      vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close Directory"))
+      vim.keymap.set("n", "v", api.node.open.vertical, opts("Open: Vertical Split"))
+    end
+
     nvimtree.setup({
+      on_attach = my_on_attach,
       view = {
         width = 35,
         relativenumber = true,
@@ -49,8 +64,13 @@ return {
     local keymap = vim.keymap -- for conciseness
 
     keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
+    keymap.set(
+      "n",
+      "<leader>ef",
+      "<cmd>NvimTreeFindFileToggle<CR>",
+      { desc = "Toggle file explorer on current file" }
+    ) -- toggle file explorer on current file
     keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
     keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
-  end
+  end,
 }
