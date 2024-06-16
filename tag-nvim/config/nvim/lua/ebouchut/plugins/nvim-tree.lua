a0173c18be8a029c8322d3fb4c892a203ddd0e18
Eric Bouchut <ebouchut@gmail.com>
2024-06-16 19:19:24 +0200

    🔧 Nvim: Update nvim-tree, add ? keyboard shortcut
    to display help

--- tag-nvim/config/nvim/lua/ebouchut/plugins/nvim-tree.lua
+++ tag-nvim/config/nvim/lua/ebouchut/plugins/nvim-tree.lua
@@ -17,6 +17,7 @@ return {
 
       api.config.mappings.default_on_attach(bufnr)
 
+      vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
       vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
       vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close Directory"))
       vim.keymap.set("n", "v", api.node.open.vertical, opts("Open: Vertical Split"))
