f1f3662a03b253fc8a9b3209e0ea379dd22b6d2b
Eric Bouchut <ebouchut@gmail.com>
2024-09-28 17:43:42 +0200

    🔧 Wezterm: Reduce font size to 14

--- tag-wezterm/config/wezterm/wezterm.lua
+++ tag-wezterm/config/wezterm/wezterm.lua
@@ -9,7 +9,7 @@ local config = wezterm.config_builder()
 config.color_scheme = "Batman"
 
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font_size = 16
+config.font_size = 14
 
 config.enable_tab_bar = false
 
